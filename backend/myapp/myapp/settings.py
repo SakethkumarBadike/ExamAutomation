79de7a82a419648354a13998477ce59fd0a9382f
SakethKumarBadike <423113@student.nitandhra.ac.in>
2026-07-01 12:34:03 +0530

    add options

--- backend/myapp/myapp/settings.py
+++ backend/myapp/myapp/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
         
     }
 }
